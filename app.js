c61b4f29c5e2590717c8a4ec7871e712d6974a66
Michael North <michael.l.north@gmail.com>
2017-08-27 16:19:25 -0700

    initial frame

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var viewEngine = require('ejs-mate');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -13,6 +14,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.engine('ejs', viewEngine);
+app.set('view options', { layout: true });
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
