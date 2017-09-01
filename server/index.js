e5718a515e1102e14dc541a6eda6a6312d9695e6
Michael North <michael.l.north@gmail.com>
2017-08-31 23:23:27 -0700

    cors

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const index = require('./routes/index');
 const accounts = require('./routes/accounts');
@@ -30,6 +31,7 @@ app.set('view options', { layout: true });
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, '..', 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser('secret'));
