772be80bdd5d06162e776fcb61037aa2681539ea
Michael North <michael.l.north@gmail.com>
2017-09-11 23:51:40 -0500

    Nodemon

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,7 +7,7 @@ router.get('/:username', function(req, res, next) {
   let { username } = req.params;
   bounceOutIfLoggedOut(req, res, () => {
     Db.query("SELECT id, username, createdAt FROM users WHERE username = '" + username + "';").spread((results) => {
-      res.render('user', {title: 'User', user: results[0] });
+      res.render('user', {title: 'User', user: results[0] || username });
     })
     .catch(() => {
       req.session.sessionFlash = {
